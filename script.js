f036e49761aa0978bcf15e626e08b3caacc1c992
Massimo Moffa <36776851+MaxMoffa@users.noreply.github.com>
2019-09-24 00:04:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -160,29 +160,27 @@ var colors = [
     'brown'
 ]
 
+var days = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+
 $("body").ready(function(){
     var today = new Date().getDay();
-    if(timetable[today].length!=0){
-        timetable[today].forEach(function(lesson){
-            $("#page-1 .body").append(createCard(lesson, colors[Math.round(Math.random() * 6)]));
-        });
-    }else{
-        $("#page-1 .body").append('<img class="empty" src="empty.png" alt="Ops! Sembra non esserci niente">');
+    var dayCounter = today;
+    for(var i=0; i<7; i++){
+        var day = $('<section id="page-' + i + '" class="page"><div class="content"><div class="head"><span>...</span></div><div class="body"></div></div></section>');
+        if(i==0) day.find("span").html("Oggi");
+        else if(i==1) day.find("span").html("Domani");
+        else day.find("span").html(days[dayCounter]);
+        if(timetable[dayCounter].length!=0){
+            timetable[dayCounter].forEach(function(lesson){
+                day.find(".body").append(createCard(lesson, colors[Math.round(Math.random() * 6)]));
+            });
+        }else{
+            day.find(".body").append('<img class="empty" src="empty.png" alt="Ops! Sembra non esserci niente">');
+        }
+        $(".page-selector").append(day);
+        dayCounter++;
+        if(days[dayCounter] == undefined) dayCounter = 0;
     }
-    if(timetable[today+1].length!=0){
-        var colorTomorrow = Math.round(Math.random() * 6)
-        timetable[today+1].forEach(function(lesson){
-            $("#page-2 .body").append(createCard(lesson, colors[Math.round(Math.random() * 6)]));
-        });
-    }else{
-        $("#page-2 .body").append('<img class="empty" src="empty.png" alt="Ops! Sembra non esserci niente">');
-    }
-    var colorOther = Math.round(Math.random() * 6)
-    timetable.forEach(function(table){
-        table.forEach(function(lesson){
-            $("#page-3 .body").append(createCard(lesson, colors[Math.round(Math.random() * 6)]));
-        });
-    });
 });
 
 function createCard(lesson, color){
